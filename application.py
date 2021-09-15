836b73f396343a4184e55c7a75e7284599064134
Harshit Agrawal <harshitagrawal.iitr@gamil.com>
2021-09-15 22:19:34 +0530

    error messages added

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ import time
 application = Flask(__name__)
 
 s3 = boto3.resource('s3')
-data = json.dumps({ "key": "value" })
+# data = json.dumps({ "key": "value" })
 
 @application.route('/')
 def intro():
@@ -32,9 +32,7 @@ def log():
         return json.dumps({ 'message': 'json.loads' })
 
     try:
-        data_dict = { 'key': 'value' }
-        # s3.Bucket('test-flask-server').put_object(Key=f'{int(time.time()*1000)}.json', Body=data_dict)
-        s3.Bucket('test-flask-server').put_object(Key=f'data.json', Body=data_dict)
+        s3.Bucket('test-flask-server').put_object(Key=f'{int(time.time()*1000)}.json', Body=json.dumps(data_dict))
     except:
         return json.dumps({ 'message': 's3.Bucket' })
 
