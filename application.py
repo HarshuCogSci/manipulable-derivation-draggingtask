b5a3149ad079eecefa1a818e2cd80a057293533c
Harshit Agrawal <harshitagrawal.iitr@gamil.com>
2021-09-15 22:12:22 +0530

    error messages added

--- application.py
+++ application.py
@@ -23,10 +23,19 @@ def finish():
 def log():
     try:
         data_json = request.form['data']
+    except:
+        return json.dumps({ 'message': 'request.form' })
+
+    try:
         data_dict = json.loads(data_json)
+    except:
+        return json.dumps({ 'message': 'json.loads' })
+
+    try:
         s3.Bucket('test-flask-server').put_object(Key=f'{int(time.time()*1000)}.json', Body=data_dict)
     except:
-        return json.dumps({ 'message': 'error' })
+        return json.dumps({ 'message': 's3.Bucket' })
+
     return json.dumps({ 'message': 'success' })
 
 # run the app.
